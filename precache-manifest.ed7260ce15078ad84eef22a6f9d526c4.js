d5293e753afbaaafd0a6a9e6ae429135e3fdd029
Linh Truong Cong Hong <linhtch90@gmail.com>
2020-10-16 11:27:20 +0700

    Updates

---
+++ precache-manifest.ed7260ce15078ad84eef22a6f9d526c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf8185163d5aa8d35dde181e9480ccd",
+    "url": "/react-covid-plot/index.html"
+  },
+  {
+    "revision": "68b945e0e70a3d4c005c",
+    "url": "/react-covid-plot/static/css/main.c0350be3.chunk.css"
+  },
+  {
+    "revision": "1c30ac40c6da7c7b711e",
+    "url": "/react-covid-plot/static/js/2.6763f17c.chunk.js"
+  },
+  {
+    "revision": "c83eaf460a8f55ef068d9679da1b7145",
+    "url": "/react-covid-plot/static/js/2.6763f17c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b945e0e70a3d4c005c",
+    "url": "/react-covid-plot/static/js/main.31af1300.chunk.js"
+  },
+  {
+    "revision": "ed3b7acebccda235676f",
+    "url": "/react-covid-plot/static/js/runtime-main.fa26f7ba.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-covid-plot/static/media/logo.ee7cd8ed.svg"
+  }
+]);
